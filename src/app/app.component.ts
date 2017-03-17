fbfa3f441189027ebda8127a13f9b099e155900c
Robert Polevoi <robert.polevoi@gmail.com>
2017-03-17 16:09:32 +0000

    working version, lacks sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { DemoService } from './demo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-}
+  
+    subscription:Subscription;
+  
+    constructor(
+              private demoServ: DemoService,
+              private router: Router) {
+      }
+  
+  next() {
+    console.log("current in next is", this.demoServ.current );
+    if (this.demoServ.current < this.demoServ.length - 1) 
+        {
+          this.router.navigate(['demo', this.demoServ.current + 1]);
+      }
+
+
+    else this.router.navigate(['demo', 0]);
+  }
+   
+     ngOnDestroy() {
+    
+    this.subscription.unsubscribe();
+  }
+  
+} 
